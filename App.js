d89bd13a21a65f666bf0f28364983dd6aa40d0fe
khaledghaffarii <med.khaledghaffary@gmail.com>
2021-11-05 12:43:33 +0100

    refactor: install the necessary dependecy for thi barnch with npm

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {View} from 'react-native';
 import {Provider} from 'react-redux';
 import {store} from './store.js';
 import HomeScreen from './screens/HomeScreen';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 function App() {
   return (
