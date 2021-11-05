724a7390e8c4240c1356c112accc77f6c14f78e6
khaledghaffarii <med.khaledghaffary@gmail.com>
2021-11-05 15:30:06 +0100

    refactor: use SafeAreaView for replace to View in HomeScreen Screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {View, Text, StyleSheet, Image} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import tw from 'tailwind-react-native-classnames';
 import NavOption from '../components/NavOption';
 const HomeScreen = () => {
   return (
-    <View style={tw`bg-white`}>
+    <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
         <Image
           style={{width: 100, height: 100, resizeMode: 'contain'}}
@@ -14,7 +15,7 @@ const HomeScreen = () => {
         />
       </View>
       <NavOption />
-    </View>
+    </SafeAreaView>
   );
 };
 
