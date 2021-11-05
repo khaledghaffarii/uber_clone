addd6b7b4e795a727d283c0a9092f55c871039b0
khaledghaffarii <med.khaledghaffary@gmail.com>
2021-11-05 12:10:20 +0100

    refactor: remove what is not needed and corvair to all screens by SafeAreaProvider in app.js

--- App.js
+++ App.js
@@ -1,54 +1,19 @@
 import React from 'react';
 
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {View} from 'react-native';
 import {Provider} from 'react-redux';
 import {store} from './store.js';
 import HomeScreen from './screens/HomeScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 function App() {
   return (
     <Provider store={store}>
-      <View>
+      <SafeAreaProvider>
         <HomeScreen />
-      </View>
+      </SafeAreaProvider>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-    flex: 1,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
